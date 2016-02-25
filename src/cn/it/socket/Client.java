2102999282e0b3472d54a14f806d9ef1c4e1c537
HEWQ <wenqiu.he@yahoo.com>
2016-02-26 00:39:52 +0800

    云端改变灯状态同时向网关发送灯的信息

--- src/cn/it/socket/Client.java
+++ src/cn/it/socket/Client.java
@@ -10,37 +10,50 @@ import java.net.Socket;
 
 public class Client {
 
- public  void start() {
-  Socket socket = null;
-  BufferedReader br = null;
-  PrintWriter pw = null;
-  try {
-   //客户端socket指定服务器的地址和端口号
-   socket = new Socket("127.0.0.1", 1234);
-   System.out.println("Socket=" + socket);
-   //同服务器原理一样
-   br = new BufferedReader(new InputStreamReader(
-     socket.getInputStream()));
-   pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
-     socket.getOutputStream())));
-    pw.println("howdy0\0" );
-    pw.flush();
-    String str = br.readLine();
-    System.out.println(str);
-   pw.println("END");
-   pw.flush();
-  } catch (Exception e) {
-   e.printStackTrace();
-  } finally {
-   try {
-    System.out.println("close......");
-    br.close();
-    pw.close();
-    socket.close();
-   } catch (IOException e) {
-    // TODO Auto-generated catch block
-    e.printStackTrace();
-   }
-  }
- }
+	private String lights = null;
+	private String roomnums = null;
+	public Client() {
+
+	}
+	
+	public Client(String light,String roomnum){
+		lights = light;
+		roomnums = roomnum; 
+	}
+
+	public void start() {
+		Socket socket = null;
+		BufferedReader br = null;
+		PrintWriter pw = null;
+		String send = null;
+		try {
+			// 客户端socket指定服务器的地址和端口号
+			socket = new Socket("127.0.0.1", 1234);
+			System.out.println("Socket=" + socket);
+			// 同服务器原理一样
+			br = new BufferedReader(new InputStreamReader(
+					socket.getInputStream()));
+			pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
+					socket.getOutputStream())));
+			send = "11" + lights + ":" + roomnums;
+			pw.println(send);
+			pw.flush();
+			String str = br.readLine();
+			System.out.println(str);
+			pw.println("END");
+			pw.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				System.out.println("close......");
+				br.close();
+				pw.close();
+				socket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
--- src/cn/it/web/controller/UpdateLightStateServlet.java
+++ src/cn/it/web/controller/UpdateLightStateServlet.java
@@ -4,6 +4,13 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import cn.it.dao.IntelligentDao;
 import cn.it.dao.impl.IntelligentDaoImpl;
+import cn.it.socket.Client;
 import cn.it.utils.DBUtil;
 
 public class UpdateLightStateServlet extends HttpServlet {
@@ -23,11 +31,21 @@ public class UpdateLightStateServlet extends HttpServlet {
 		String lightLocation = lr[0];
 		String roomNum = lr[1];
 		String roomBuilding = roomNum.substring(0, 1);
-		// 获取灯的状态
+		String lights_num4room = null;
+		String lightState = null;
+		String alllight = null;
+		String key = null;
+		String value =null;
+		String light;
+		Map<String,String> map = new LinkedHashMap<String,String>();
+		// 根据灯的位置获取灯的状态
 		String sql_lightState = "select lights_state from lights where lights_location="
 				+ "'"
 				+ lightLocation
 				+ "' and lights_room = (select room_id from room where room_num = '"
+				+ roomNum + "')";
+		// 获取灯的编号
+		String sql_lightsnum4room = "select lights_num4room from lights where lights_room = (select room_id from room where room_num = '"
 				+ roomNum + "')";
 		// 开灯
 		String sql_open = "update lights set lights_state = 1 where lights_location="
@@ -41,24 +59,50 @@ public class UpdateLightStateServlet extends HttpServlet {
 				+ lightLocation
 				+ "' and lights_room = (select room_id from room where room_num = '"
 				+ roomNum + "')";
-		
+
 		IntelligentDao dao = new IntelligentDaoImpl();
 		// 返回灯的状态
 		ResultSet rs_lightState = dao.select(sql_lightState);
+		// 该灯在房间内的编号
+		ResultSet rs_lightsnum4room = dao.select(sql_lightsnum4room);
 		try {
 			while (rs_lightState.next()) {
-				if (rs_lightState.getString("lights_state").equals("1")) {	//已经开着灯
-					dao.update(sql_close);//关灯
-				}else{
-					dao.update(sql_open);	//开灯
+				if (rs_lightState.getString("lights_state").equals("1")) { // 已经开着灯
+					dao.update(sql_close);// 关灯
+				} else {
+					dao.update(sql_open); // 开灯
+				}
+			}
+			while (rs_lightsnum4room.next()) {
+				lights_num4room = rs_lightsnum4room
+						.getString("lights_num4room");
+				// 根据灯的编号获取灯的状态
+				String sql_lightState4lightsnum = "select lights_state from lights where lights_num4room="
+						+ "'"
+						+ lights_num4room
+						+ "' and lights_room = (select room_id from room where room_num = '"
+						+ roomNum + "')";
+				// 返回灯的状态
+				ResultSet rs_lightState4lightsnum = dao.select(sql_lightState4lightsnum);
+				while (rs_lightState4lightsnum.next()) {
+					lightState = rs_lightState4lightsnum
+							.getString("lights_state");
+					map.put(lights_num4room, lightState);
 				}
 			}
 			request.setAttribute("room_num", roomNum);
 			request.setAttribute("room_building", roomBuilding);
 			request.getRequestDispatcher("/WEB-INF/jsp/inRoom.jsp").forward(
 					request, response);
+			for(int i = 0;i < map.size();i++){
+				key = String.valueOf(i);
+				value = map.get(key);
+				alllight = alllight + key + value;
+			}
+			light = alllight.substring(4);
+			Client client = new Client(light,roomNum);
+			// client.start();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
 			dao.close();
